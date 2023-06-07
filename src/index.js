146a75336d2ad7dd41376efadb5b4c0d81b2b8f8
IbrahimMalgwi <ibrahimganamalgwi@outlook.com>
2023-06-07 22:18:38 +0100

    Hooks in React js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Header from './Header';
 import User from './User';
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <>
       <User name="Ibbi" expertise="Software Engineering" />
-
-      <User name="Mike" expertise="Python" />
-
-      <User name="Jane" expertise="Java" />
+      <App />
+      
     </>
   </React.StrictMode>
 );
